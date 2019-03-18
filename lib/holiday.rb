9a4efa5a7adf9c94a15953af236df317a895512d
Ernie Val <erndval@gmail.com>
2019-03-18 21:47:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,7 +48,6 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash.each do |season, holiday|
     if season == :winter
         supplies << december_things
-      end
     end
   end
     december_things.join
