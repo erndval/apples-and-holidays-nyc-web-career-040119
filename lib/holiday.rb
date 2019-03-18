a3d0d900d612e03625ddd85285ba795ae460b80c
Ernie Val <erndval@gmail.com>
2019-03-18 22:23:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,9 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+holiday_hash.each |season, holiday|
+  holiday.each |holiday, supplies|
+  if supplies
 end
 
 
